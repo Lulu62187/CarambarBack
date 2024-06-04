87468c07f5278253e86190feb7e650ea4f6c0b6e
lulu62187 <lucasdachicourt2@gmail.com>
2024-06-04 15:37:43 +0200

    ne plus toucher

--- serveur.js
+++ serveur.js
@@ -5,15 +5,17 @@ const router = require('./routes/');
 const version = "v1"
 const db = require('./db/database');
 const cors = require("cors");
- 
+const swaggerDocument = require('./swagger.json');
+const swaggerUi = require('swagger-ui-express')
+
+
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(`/api/${version}`, router);
 
-
-
+app.use(`/api-docs`, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 db.sync().then(() => {
     console.log('DBConnect est synchronisé')
     app.listen(port, () => {
